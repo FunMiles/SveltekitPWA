f98f32b7ace98bb6d4999a2fcc9e802d600b4c7f
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-05-19 23:08:40 +0200

    chore: include WebWorker for ts chore: exclude intelllij files chore: include skip waiting on sw chore: some changes to webmanifest

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,10 +1,31 @@
 import { build, files, timestamp } from '$service-worker';
 import { precacheAndRoute } from 'workbox-precaching';
 
+declare let self: ServiceWorkerGlobalScope
+
 console.log('SW build', build, 'files', files, 'timestamp', timestamp);
 
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING')
+        self.skipWaiting()
+})
 
-// // @ts-ignore
-precacheAndRoute([...build.map(f=>{return {url: f, revision: null}}), 
-    ...files.map(f=>{return {url: f, revision: `${timestamp}`}})]);
+
+precacheAndRoute([
+    ...build.map(f => {
+        return {
+            url: f,
+            revision: null
+        }
+    }),
+    ...files.map(f => {
+        return {
+            url: f,
+            revision: `${timestamp}`
+        }
+    })
+]);
+
 console.log('Done passing things to workbox.');
+
+
