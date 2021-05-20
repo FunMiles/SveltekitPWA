6af8041637c75b7db41a188cab843c063d9af1a0
Michel Lesoinne <michel.lesoinne@androdevllc.com>
2021-05-20 08:30:37 -0600

    fix: correctly cache and route the svelte-kit routes
    Svelte-kit routes have no '.html' matching files and do not appear
    in the kit's provided list of files or build. This change adds a
    variable where the routes can be listed to be added to the list for
    caching and each is given a route with StaleWhileRevalidate
    strategy, allowing the cache to be updated yet serving the cached
    value until the new version is loaded.

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,5 +1,7 @@
 import { build, files, timestamp } from '$service-worker';
-import { precacheAndRoute } from 'workbox-precaching';
+import { precacheAndRoute, precache } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import {StaleWhileRevalidate} from 'workbox-strategies';
 
 declare let self: ServiceWorkerGlobalScope
 
@@ -27,6 +29,22 @@ precacheAndRoute([
     })
 ]);
 
-console.log('Done passing things to workbox.');
+// Edit the list of routes so they get cached and routed correctly, allowing
+// cold start or hot reload to work offline.
+const skRoutes = [ '/', '/about', '/todos' ];
+
+precache( skRoutes.map(f => {
+    return {
+        url: f,
+        revision: `${timestamp}`
+    }
+}) );
+
+const matchCb = ({url, request, event}) => {
+    return skRoutes.some(path => url.pathname === path);
+};
+
+registerRoute(matchCb, new StaleWhileRevalidate({}));
+
 
 
