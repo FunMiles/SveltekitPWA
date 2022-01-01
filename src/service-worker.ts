07646c3520f77df7c67b9560ad307148dd03e949
ap-1 <pallatianish@gmail.com>
2022-01-01 14:43:06 -0500

    fixes FunMiles/SveltekitPWA#6

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,47 +1,47 @@
 import { build, files, timestamp } from '$service-worker';
 import { precacheAndRoute, precache } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import {StaleWhileRevalidate} from 'workbox-strategies';
+import { StaleWhileRevalidate } from 'workbox-strategies';
 
-declare let self: ServiceWorkerGlobalScope
+import type { RouteHandlerCallbackOptions } from 'workbox-core';
+
+declare let self: ServiceWorkerGlobalScope;
 
 self.addEventListener('message', (event) => {
-    if (event.data && event.data.type === 'SKIP_WAITING') {
-        self.skipWaiting();
-    }
+	if (event.data && event.data.type === 'SKIP_WAITING') {
+		self.skipWaiting();
+	}
 });
 
-
 precacheAndRoute([
-    ...build.map(f => {
-        return {
-            url: f,
-            revision: null
-        }
-    }),
-    ...files.map(f => {
-        return {
-            url: f,
-            revision: `${timestamp}`
-        }
-    })
+	...build.map((f) => {
+		return {
+			url: f,
+			revision: null
+		};
+	}),
+	...files.map((f) => {
+		return {
+			url: f,
+			revision: `${timestamp}`
+		};
+	})
 ]);
 
 // Edit the list of routes so they get cached and routed correctly, allowing
 // cold start or hot reload to work offline.
-const skRoutes = [ '/', '/about', '/todos' ];
+const skRoutes = ['/', '/about', '/todos'];
 
-precache( skRoutes.map(f => {
-    return {
-        url: f,
-        revision: `${timestamp}`
-    }
-}) );
+precache(
+	skRoutes.map((f) => {
+		return {
+			url: f,
+			revision: `${timestamp}`
+		};
+	})
+);
 
-const matchCb = ({url, request, event}) => {
-    return skRoutes.some(path => url.pathname === path);
+const matchCb = ({ url }: RouteHandlerCallbackOptions) => {
+	return skRoutes.some((path) => url.pathname === path);
 };
 registerRoute(matchCb, new StaleWhileRevalidate({}));
-
-
-
