0d48f22fac7883f024aa6e613a4444133f5e1387
ap-1 <pallatianish@gmail.com>
2022-01-01 14:42:59 -0500

    fixes FunMiles/SveltekitPWA#6

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,16 +2,16 @@ import cookie from 'cookie';
 import { v4 as uuid } from '@lukeed/uuid';
 import type { Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ request, render }) => {
+export const handle: Handle = async ({ request, resolve }) => {
 	const cookies = cookie.parse(request.headers.cookie || '');
 	request.locals.userid = cookies.userid || uuid();
 
 	// TODO https://github.com/sveltejs/kit/issues/1046
-	if (request.query.has('_method')) {
-		request.method = request.query.get('_method').toUpperCase();
+	if (request.url.searchParams.has('_method')) {
+		request.method = request.url.searchParams.get('_method')?.toUpperCase() ?? '';
 	}
 
-	const response = await render(request);
+	const response = await resolve(request);
 
 	if (!cookies.userid) {
 		// if this is the first time the user has visited this app,
